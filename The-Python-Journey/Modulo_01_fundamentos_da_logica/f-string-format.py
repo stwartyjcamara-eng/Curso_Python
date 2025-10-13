8d8980849c1e8d7b597a60dec232a1863e4a484b
Stwarty <stwartyjcamara@hotmail.com>
2025-10-13 20:02:51 -0300

    Add string formatting examples demonstrating alignment and padding

--- The-Python-Journey/Modulo_01_fundamentos_da_logica/f-string-format.py
+++ The-Python-Journey/Modulo_01_fundamentos_da_logica/f-string-format.py
@@ -19,3 +19,30 @@ print('Meu nome é {n} ,tenho {i} anos e ' \
 print('Meu nome é {n} ,tenho {i} anos e ' \
 'minha altura é {a:.0f}'.format(n=nome, i=idade, a=altura))
 # {:.0f} -> 0 casas decimais
+
+# Formatação básica de strings
+# s - string
+# d - int
+# f - float
+# .<número de dígitos>f
+# x ou X - Hexadecimal
+# (Caractere)(><^)(quantidade)
+# > - Esquerda
+# < - Direita
+# ^ - Centro
+print (f'{nome:>10}.') # Alinha a direita
+print (f'{nome:<10}.') # Alinha a esquerda
+print (f'{nome:^10}.') # Alinha ao centro
+print (f'{nome:.^20}.') # Preenche com pontos
+print (f'{idade:0<10}.') # Preenche com zeros
+print (f'{altura:0>10.2f}.') # Preenche com zeros e 2 casas decimais
+print (f'{1000.48765:0<20,.2f}.') # Preenche com zeros, 2 casas decimais e separador de milhar
+# = - Força o número a aparecer antes dos zeros
+print (f'{1000.48765:=^20,.2f}.') # Força o número a aparecer antes dos zeros, 2 casas decimais e separador de milhar
+# + - Sempre mostra o sinal
+# Sinal - + ou -
+# Ex.: 0>-100,.1f
+print (f'{1000.48765:+>100,.1f}.') # Sempre mostra o sinal, 2 casas decimais e separador de milhar
+# Conversion flags - !r !s !a 
+
+
